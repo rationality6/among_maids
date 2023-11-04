3c8104d2bff830ca17732bead92a6dcf736efe73
Hyun Ahn <codepawn@icloud.com>
2023-11-04 15:06:56 +0900

    express update

---
+++ server/index.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+const { Server } = require("socket.io");
+
+// module.exports = app;
+
+const createApp = () => {
+  //   // logging middleware
+  app.use(morgan("dev"));
+
+  //   // body parsing middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  //   // compression middleware
+  app.use(compression());
+
+  //   // static file-serving middleware
+  app.use(express.static(path.join(__dirname, "..", "dist")));
+
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error("Not found");
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
+
+  //   // sends index.html
+  app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "dist/index.html"));
+  });
+
+    app.use("/foo", (req, res, next) => {
+      console.log("foo");
+      console.log("foo");
+    });
+
+  //   // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
+};
+
+const startListening = () => {
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  );
+  const io = new Server(server);
+
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  });
+};
+
+async function bootApp() {
+  await createApp();
+  await startListening();
+}
+
+bootApp();
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,10 +6,21 @@ class GameScene extends PhaserSceneTool {
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'interpretLogoWithCat');
+    const logo = this.add.image(200, 70, 'interpretLogoWithCat');
 
     this.tweens.add({
       targets: logo,
+      y: 350,
+      duration: 1500,
+      ease: 'Sine.inOut',
+      yoyo: true,
+      repeat: -1
+    });
+
+    const logo2 = this.add.image(500, 170, 'interpretLogoWithCat');
+
+    this.tweens.add({
+      targets: logo2,
       y: 350,
       duration: 1500,
       ease: 'Sine.inOut',
